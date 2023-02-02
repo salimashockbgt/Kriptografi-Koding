14e8c04ad9ed5df76e72edb05c1e20e2fc31c817
Shely <88304373+celiii26@users.noreply.github.com>
2023-02-02 10:45:24 +0700

    Update extended.py

--- extended.py
+++ extended.py
@@ -1,25 +1,4 @@
-'''def generateKey(string, key): 
-	key = list(key) 
-	if len(string) == len(key): 
-		return(key) 
-	else: 
-		for i in range(len(string) - len(key)): 
-			key.append(key[i % len(key)]) 
-	return("" . join(key)) 
-
-def encryptText(plain_text, key):
-    cipher = [[0] for i in range(len(plain_text))]
-    key = generateKey(plain_text, key)
-    key = key.strip().upper()
-    keyIndex = 0
-    for i in range (len(plain_text)):
-        keyIndex = keyIndex % len(key)
-        shift = ord(key[keyIndex]) - 65
-        cipher[i] = chr((ord(plain_text[i]) + shift) % 256)
-        keyIndex += 1
-    return "".join(cipher)
-
-print(encryptText("thisplaintext", "sony"))'''
+# Extended Vigenere Cipher (256 Character ASCII)
 
 def extended_vigenere_encrypt(text, key):
     # convert text to list of char
@@ -35,7 +14,7 @@ def extended_vigenere_encrypt(text, key):
 
     return ("".join(encrypted_text))
 
-print(extended_vigenere_encrypt("HALOGUYS123@gmail", "sony"))
+# print(extended_vigenere_encrypt("HALOGUYS123@gmail", "sony"))
 
 def extended_vigenere_decrypt(encrypted_text, key):
     # convert text to list of char
@@ -51,4 +30,47 @@ def extended_vigenere_decrypt(encrypted_text, key):
 
     return ("".join(plain_text))
 
-print(extended_vigenere_decrypt("»°ºÈºÄÇÌ¤¡¡¹ÚÜÏâß", "sony"))
+# print(extended_vigenere_decrypt("»°ºÈºÄÇÌ¤¡¡¹ÚÜÏâß", "sony"))
+
+def encryptByteExtendedVigenere(bytePlainText, key):
+    result = [[0] for i in range(len(bytePlainText))]
+    key = key.strip().upper()
+    keyIndex = 0
+    keylength = len(key)
+    for i in range(len(bytePlainText)):
+        keyIndex = keyIndex % keylength
+        shift = ord(key[keyIndex]) - 65
+        result[i] = bytes([(ord(bytePlainText[i]) + shift) % 256])
+        keyIndex+=1
+    return result
+
+def decryptByteExtendedVigenere(byteCipherText, key):
+    result = [[0] for i in range(len(byteCipherText))]
+    key = key.strip().upper()
+    keyIndex = 0
+    keylength = len(key)
+    for i in range(len(byteCipherText)):
+        keyIndex = keyIndex % keylength
+        shift = ord(key[keyIndex]) - 65
+        result[i] = bytes([(ord(byteCipherText[i]) + 256 - shift) % 256]).decode("utf-8")
+        keyIndex+=1
+    return result
+
+def write_to_file(path, text):
+    file1 = open(path,"w+") 
+    file1.write(text) 
+    file1.close()
+
+def readTextFromFile(path):
+    file1 = open(path, "r")
+    data = file1.read()
+    file1.close()
+    return data
+
+data = readTextFromFile("plainText/text.txt")
+print(data)
+encrypt = encryptByteExtendedVigenere(data, "sony")
+print(encryptByteExtendedVigenere(data, "sony"))
+print(decryptByteExtendedVigenere(encrypt, "sony"))
+decrypt = "".join(decryptByteExtendedVigenere(encrypt, "sony"))
+write_to_file("cipherText/hasil.txt", decrypt)
