0730888d9fe27b2fc22b1243f69481e035b1074a
salimashockbgt <76551955+salimashockbgt@users.noreply.github.com>
2023-02-03 12:23:21 +0700

    Update onetimepad.py

--- onetimepad.py
+++ onetimepad.py
@@ -1,56 +1,47 @@
 from os import listdir, path
 from random import seed, randint
 
-alphabet = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z, '.split(',')
+def readTextFromFile(path):
+    file1 = open(path, "r")
+    data = file1.read()
+    file1.close()
+    return data
 
-def shiftLetter(letter, shift):
-    newIdx = alphabet.index(letter) + shift
+wordCollection = readTextFromFile('C:/Users/Lenovo/kripto/abc.txt')
+#Menghasilkan kunci yang di-randomize secara acak
+def generateKey(text):
+    key = ''
+    for i in range(len(text)):
+        seed()
+        key += wordCollection[randint(0,26)]
+    return key
+def geserHuruf(letter, shift):
+    newIdx = wordCollection.index(letter) + shift
     if newIdx >= 27:
-        return alphabet[newIdx-27]
+        return wordCollection[newIdx-27]
     elif newIdx >= 0 and newIdx < 27:
-       return alphabet[newIdx]
+       return wordCollection[newIdx]
     else:
-        return alphabet[27+newIdx]
+        return wordCollection[27+newIdx]
 
-def vigenere(text, key, cipher):
+def crypto(text, key, cipher):
     keyShifts = []
     if cipher:
-        # keyShifts = all shifts in key ('gkoy' becomes [6, 10, 14, 24] for each letter respectively) (encryption)
-        keyShifts = [ (alphabet.index(key[i]) - alphabet.index('a')) for i in range(len(key)) ]
+        # Mengubah huruf-huruf pada wordCollection menjadi koleksi urutan setiap huruf yang dikurangi 1
+        for i in range(len(key)):
+            keyShifts.append(wordCollection.index(key[i]) - wordCollection.index('a'))
     else:
-        # keyShifts = all shifts in key, but negative ('gkoy' becomes [-6, -10, -14, -24] for each letter respectively) (decryption)
-        keyShifts = [ ((alphabet.index(key[i]) - alphabet.index('a')) * -1) for i in range(len(key)) ]
-    
-    textLetters = [ text[i] for i in range(len(text)) ] # parse all characters in text into an array
-    cipherKey = [ keyShifts[i % len(keyShifts)] for i in range(len(textLetters)) ]  # repeat keyShifts to match textLetters length ('hello' with key 'gk' will output [6, 10, 6, 10, 6])
-    vigenereText = [ shiftLetter(textLetters[i], cipherKey[i]) for i in range(len(textLetters)) ]   # shift every letter to encrypt/decrypt text
+        # sama seperti kondisi if, tetapi dalam bentuk negatif
+        keyShifts = [ ((wordCollection.index(key[i]) - wordCollection.index('a')) * -1) for i in range(len(key)) ]
+    textLetters=[]
+    cipherKey=[]
+    for i in range(len(text)):
+        textLetters.append(text[i])
+    # memasukkan setiap karakter dalam text menjadi array of strings
+    # mengulang keyShifts supaya bisa dicocokkan dengan textLetters length ('hello' dengan key 'gk' akan menghasilkan [6, 10, 6, 10, 6])
 
-    return ''.join(vigenereText)
+    cipherKey = [ keyShifts[i % len(keyShifts)] for i in range(len(textLetters)) ]
+    onetimepadText = [ geserHuruf(textLetters[i], cipherKey[i]) for i in range(len(textLetters)) ]   
+    # shift every letter to encrypt/decrypt text
 
-
-if __name__ == "__main__":
-    print('One Time Pad')
-    print('------------------')
-    print('1. Cipher a message')
-    print('2. Decipher a message')
-    choice = input('\nYour Choice: ')
-
-    if choice == '1':
-        print('\nPlease input the plaintext you want to cipher')
-        plaintext = input('Plain Text: ')
-
-        key = ''
-        for i in range(len(plaintext)):
-            seed()
-            key += alphabet[randint(0,26)]
-
-        cipherText = vigenere(plaintext, key, True)
-        print('\nYour ciphertext is: ', cipherText)
-        print('Your key is: ', key)
-    else:
-        print('\nPlease input the ciphertext you want to decipher and the key')
-        ciphertext = input('Cipher Text: ')
-        key = input('Key: ')
-
-        plainText = vigenere(ciphertext, key, False)
-        print('\nYour plaintext is: ', plainText)
+    return ''.join(onetimepadText)
